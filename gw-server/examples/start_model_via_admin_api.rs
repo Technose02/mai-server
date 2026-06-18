f4900aed89f66c708afa3153f1209ea3c38536ba
technose02 <technose02@yahoo.de>
2026-06-18 06:25:06 +0200

    wip

--- gw-server/examples/start_model_via_admin_api.rs
+++ gw-server/examples/start_model_via_admin_api.rs
@@ -2,6 +2,8 @@ use axum::http::Method;
 use rig::http_client::ReqwestClient;
 use serde_json::json;
 
+const BASE_URL: &str = "https://mai-server.ipv64.net:8080";
+
 #[tokio::main]
 async fn main() {
     let apikey = {
@@ -11,29 +13,36 @@ async fn main() {
 
 
     let client = ReqwestClient::new();
-    let r = client.request(Method::PUT, "https://mai-server.ipv64.net:8080/admin/llamacpp/llm")
+    let r = client.request(Method::PUT, format!("{BASE_URL}/admin/llamacpp/llm"))
     .header("Authorization", format!("Bearer {apikey}"))
     .json(&json!({
         "env": {
             "GGML_CUDA_ENABLE_UNIFIED_MEMORY" : "1"
         },
         "ctx-size" : 131072,
-        "alias": "qwen3.6-35b-a3b-mtp-thinking-precise-coding",
-        "model-path": "/model_data/huggingface/unsloth/Qwen3.6-35B-A3B-MTP-GGUF/BF16/Qwen3.6-35B-A3B-BF16-00001-of-00002.gguf",
-        "mmproj-path": "/model_data/huggingface/unsloth/Qwen3.6-35B-A3B-MTP-GGUF/mmproj-BF16.gguf",
-        "n-gpu-layers": 99,
+        "alias": "qwen3.6-27b-agentic-coding-no-reasoning",
+        "model-path": "/model_data/huggingface/unsloth/Qwen3.6-27B-MTP-GGUF/Qwen3.6-27B-UD-Q8_K_XL.gguf",
+        "mmproj-path": "/model_data/huggingface/unsloth/Qwen3.6-27B-MTP-GGUF/mmproj-BF16.gguf",
+        "n-gpu-layers": -1,
         "flash-attn": "on",
-        "batch-size": 1024,
+        "batch-size": 512,
+        "ubatch-size": 2048,
+        "cache-type-v": "q8_0",
+        "cache-ram": -1,
+        "cache-reuse": 64,
         "spec-type": "draft-mtp",
         "spec-draft-n-max": 2,
-        "temp": 0.6,
+        "cache-type-k": "q8_0",
+        "temp": 0.3,
         "repeat-penalty": 1.0,
         "presence-penalty": 0.0,
         "min-p": 0.0,
         "top-k": 20,
         "top-p": 0.95,
         "jinja": true,
-        "mlock": true
+        "mlock": true,
+        "chat-template-kwargs": "{\"preserve_thinking\":\"true\"}",
+        "reasoning": "off"
     })).build().unwrap();
     client.execute(r).await.unwrap();
 }
