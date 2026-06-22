8ac07bc03e0366c57787630ae001d86036cbf33a
technose02 <technose02@yahoo.de>
2026-06-22 06:33:56 +0200

    updated

--- gw-server/examples/laurin_ocr.rs
+++ gw-server/examples/laurin_ocr.rs
@@ -1,12 +1,18 @@
-use std::path::PathBuf;
+use std::{io::Write, path::PathBuf};
 
+//use axum::extract::ws::Message;
 use base64::{Engine, prelude::BASE64_STANDARD};
 use dotenv;
-use rig::{
+use futures::StreamExt;
+use image::GenericImageView;
+use rig_core::{
+    OneOrMany,
+    agent::MultiTurnStreamItem,
     client::CompletionClient,
-    completion::Prompt,
     http_client::ReqwestClient,
-    message::{DocumentSourceKind, Image, ImageDetail, ImageMediaType},
+    message::{DocumentSourceKind, Image, ImageDetail, ImageMediaType, Message, UserContent},
+    providers::openai::CompletionsClient,
+    streaming::StreamingPrompt,
 };
 use serde_json::json;
 use tracing::info;
@@ -16,6 +22,9 @@ const SCANNED_PAGES_DIR: &str = "/data0/dev/python/laurins-geheimnis-md/data/ima
 const OUT_DIR: &str = "/home/technose02/Documents/laurin_ocr_out/take2";
 const SEED: u64 = 2;
 
+//const MODEL: &str = "qwen3.6-27b-agentic-coding-no-reasoning-60000";
+const MODEL: &str = "qwen3.6-27b-mtp-ud-q8-k-xl-non-thinking-reasoning-200000";
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
@@ -24,12 +33,12 @@ async fn main() {
     let apikey = std::env::var(MAI_SERVER_APIKEY)
         .unwrap_or_else(|_| panic!("env-var {MAI_SERVER_APIKEY} not set"));
 
-    let client = rig::providers::openai::client::CompletionsClient::<ReqwestClient>::builder()
+    let client = CompletionsClient::<ReqwestClient>::builder()
         .base_url("https://mai-server.ipv64.net:8080/api/v1")
         .api_key(apikey)
         .http_client(
             ReqwestClient::builder()
-                .timeout(std::time::Duration::from_millis(300000))
+                .timeout(std::time::Duration::from_millis(u64::MAX))
                 .build()
                 .expect("expected building a reqwest-client to succeed"),
         )
@@ -37,12 +46,15 @@ async fn main() {
         .expect("failed to build openai-client");
 
     let agent = client
-        .agent("qwen3.5-35b-a3b-non-thinking-general-min")
+        .agent(MODEL)
         .preamble(
             r#"Du bist ein Experte für die Erstellung von E-Books als Markdown-Formatierte Textseiten aus
 eingescannten Buchseiten im PNG-Format.
 Du erhältst von dem User gleich den Scan einer Buchseite des deutschen Romans "Laurins Geheimnis".
 Gib den Inhalt dieser Seite als Text aus. Erhalte die Formatierung und Struktur.
+Als Hilfe für die Einordnung des ermittelten Textes erhältst du mit dem Bild der Buchseite zusätzlich den
+Text der vorherigen Seite, an den der neue Text anschließt.
+
 Verwende Markdown für die Formatierung und nutze die in Markdown verfügbaren Möglichkeiten:
 - nutze Überschriften für neue Kapitel
 - nutze kursive und fett gedruckte Schrift innerhalb des Textflusses, wenn das original entsprechende Hervorhebungen nutzt
@@ -63,23 +75,87 @@ Antworte ausschließlich mit dem generierten Text."#,
     if let Err(e) = std::fs::create_dir_all(&laurin_ocr_out) {
         panic!("could not create dir(s) '{:#?}': {e}", &laurin_ocr_out)
     }
+
+    let mut markdown_content = String::new();
+    let mut content_previous_page = Option::None;
     loop {
         if let Some((image, path)) = load_image(idx as usize) {
             info!("analyzing image {path:#?}...");
-            match agent.prompt(image).await {
-                Ok(res) => {
-                    info!("\twriting output to {path:#?}...");
-                    tokio::fs::write(laurin_ocr_out.join(&format!("page_{:03}.md", idx)), res)
-                        .await
-                        .unwrap_or_else(|e| panic!("error writing output as md-file: {e}"));
-                    idx += 1;
-                    info!("\tdone");
-                    continue;
-                }
-                Err(e) => {
-                    panic!("{e}")
+
+            let mut usercontent = if let Some(content_previous_page) = content_previous_page {
+                OneOrMany::one(UserContent::text(format!(
+                    r#"Dies ist der Text der vorherigen Seite:
+    <TEXT_VORHERIGE_SEITE>
+    {content_previous_page}
+    </TEXT_VORHERIGE_SEITE>
+    Die nächste Seite ist als Bild angefügt. Generiere nun den Text der nächsten Seite.
+    "#
+                )))
+            } else {
+                OneOrMany::one(UserContent::text(
+                    r#"Die erste Seite des Buches ist als Bild angefügt. Es gibt keine vorherige Seite. Generiere nun den Text der nächsten Seite."#,
+                ))
+            };
+
+            usercontent.push(UserContent::Image(image));
+
+            let mut final_response = None;
+            let mut stream = agent
+                .stream_prompt(Message::User {
+                    content: usercontent,
+                })
+                .await;
+            while let Some(n) = stream.next().await {
+                match n {
+                    Ok(MultiTurnStreamItem::FinalResponse(fr)) => final_response = Some(fr),
+                    Ok(MultiTurnStreamItem::StreamAssistantItem(_)) => {
+                        print!(".");
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Ok(_) => {}
+                    Err(e) => panic!("{e}"),
                 }
             }
+
+            if let Some(final_response) = final_response {
+                markdown_content.push_str("\n\n---\n\n");
+                markdown_content.push_str(final_response.response());
+
+                content_previous_page = Some(final_response.response().to_string());
+
+                info!("\twriting output to {path:#?}...");
+
+                tokio::fs::write(laurin_ocr_out.join("all_pages.md"), &markdown_content)
+                    .await
+                    .unwrap_or_else(|e| panic!("error updating 'all_pages.md': {e}"));
+
+                tokio::fs::write(
+                    laurin_ocr_out.join(&format!("page_{:03}.md", idx)),
+                    final_response.response(),
+                )
+                .await
+                .unwrap_or_else(|e| panic!("error writing output as md-file: {e}"));
+                idx += 1;
+                info!("\tdone");
+                continue;
+            } else {
+                panic!("no final response received!");
+            }
+
+            //match agent.prompt(image).await {
+            //    Ok(res) => {
+            //        info!("\twriting output to {path:#?}...");
+            //        tokio::fs::write(laurin_ocr_out.join(&format!("page_{:03}.md", idx)), res)
+            //            .await
+            //            .unwrap_or_else(|e| panic!("error writing output as md-file: {e}"));
+            //        idx += 1;
+            //        info!("\tdone");
+            //        continue;
+            //    }
+            //    Err(e) => {
+            //        panic!("{e}")
+            //    }
+            //}
         }
         break;
     }
@@ -90,6 +166,53 @@ Antworte ausschließlich mit dem generierten Text."#,
 fn load_image(idx: usize) -> Option<(Image, PathBuf)> {
     let src = std::path::PathBuf::from(SCANNED_PAGES_DIR).join(format!("laurin_{idx:03}.png"));
     if src.exists() && src.is_file() {
+        let mut img = image::ImageReader::open(&src)
+            .unwrap()
+            .decode()
+            .unwrap()
+            .grayscale();
+        let (mut w, mut h) = img.dimensions();
+        let scale = 1024.0 / ((w as f32).sqrt() * (h as f32).sqrt());
+        w = (scale * w as f32).round() as u32;
+        h = (scale * h as f32).round() as u32;
+        let fix_w = w % 16;
+        if fix_w >= 8 {
+            w += 16 - fix_w;
+        } else {
+            w -= fix_w;
+        }
+        let fix_h = h % 16;
+        if fix_h >= 8 {
+            h += 16 - fix_h;
+        } else {
+            h -= fix_h;
+        }
+        img = img.resize(w, h, image::imageops::FilterType::Lanczos3);
+        let mut img = img.to_luma8();
+        let mut px_min = u8::MAX;
+        let mut px_max = 0;
+        for px in img.pixels_mut() {
+            let val = px.0[0];
+            if val > px_max {
+                px_max = val;
+            }
+            if val < px_min {
+                px_min = val;
+            }
+        }
+        let t = (px_max - px_min) / 2;
+        for px in img.pixels_mut() {
+            //println!("thresholding - cur is {}, t is {t}, px_min is {px_min}, px_max is {px_max}", px.0[0]);
+            px.0[0] = if px.0[0] >= t { u8::MAX } else { 0_u8 };
+        }
+        let mut img_buf = std::io::Cursor::new(Vec::new());
+        img.write_to(&mut img_buf, image::ImageFormat::Png).unwrap();
+        let image_bytes = img_buf.into_inner();
+        let fname = src.file_name().unwrap().to_string_lossy().to_string();
+        let fname = fname.to_lowercase().replace(".png", "converted.png");
+        let processed_dst = src.parent().unwrap().to_owned().join(fname);
+        std::fs::write(processed_dst, image_bytes).unwrap();
+
         let image_bytes = std::fs::read(&src).unwrap();
         let base64_image = BASE64_STANDARD.encode(image_bytes);
 
