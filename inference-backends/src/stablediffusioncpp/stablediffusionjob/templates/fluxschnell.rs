3f01d58067dac450a529cee4d65e4ef83210e848
technose02 <technose02@yahoo.de>
2026-09-06 06:13:54 +0200

    refactored

--- inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/fluxschnell.rs
+++ inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/fluxschnell.rs
@@ -5,20 +5,21 @@ use crate::stablediffusioncpp::{
 
 impl StableDiffusionJob {
     pub fn flux_schnell_job() -> Self {
-        let mut job = StableDiffusionJob::default();
-        job.path_to_model = "/model_data/comfyui-model-base/unet/flux1-schnell-Q8_0.gguf".into();
-        job.path_to_vae = "/model_data/comfyui-model-base/vae/flux-vae.safetensors".into();
-        job.textencoder = ClipModel::clipl_and_t5xxl(
-            "/model_data/comfyui-model-base/clip/clip_l.safetensors",
-            "/model_data/comfyui-model-base/clip/t5xxl_fp8_e4m3fn.safetensors",
-        );
-        job.steps = 4;
-        job.width = 1024;
-        job.height = 1024;
-        job.cfg_scale = 1.0;
-        job.offload_to_cpu = false;
-        job.flash_attention_mode = FlashAttentionMode::Full;
-        job.prompt= "A Logo in white on black background saying 'Flux Schnell' in capitals using a classic computer terminal font. Text is centered horizontally and vertically".into();
-        job
-    }
+        Self {
+            path_to_model: "/model_data/comfyui-model-base/unet/flux1-schnell-Q8_0.gguf".into(),
+            path_to_vae: "/model_data/comfyui-model-base/vae/flux-vae.safetensors".into(),
+            textencoder: ClipModel::clipl_and_t5xxl(
+                "/model_data/comfyui-model-base/clip/clip_l.safetensors",
+                "/model_data/comfyui-model-base/clip/t5xxl_fp8_e4m3fn.safetensors",
+            ),
+            steps: 4,
+            width: 1024,
+            height: 1024,
+            cfg_scale: 1.0,
+            offload_to_cpu: false,
+            flash_attention_mode: FlashAttentionMode::Full,
+            prompt: "A Logo in white on black background saying 'Flux Schnell' in capitals using a classic computer terminal font. Text is centered horizontally and vertically".into(),
+            ..Default::default()
+        }
+    }    
 }
--- inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/zimage.rs
+++ inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/zimage.rs
@@ -5,21 +5,19 @@ use crate::stablediffusioncpp::{
 
 impl StableDiffusionJob {
     pub fn z_image_job() -> Self {
-        let mut job = StableDiffusionJob::default();
-        job.path_to_model = "/model_data/huggingface/Comfy-Org/z_image/split_files/diffusion_models/z_image_bf16.safetensors".into();
-        job.path_to_vae =
-            "/model_data/huggingface/Comfy-Org/z_image/split_files/vae/ae.safetensors".into();
-        job.textencoder = ClipModel::llm(
-            "/model_data/huggingface/Comfy-Org/z_image/split_files/text_encoders/qwen_3_4b.safetensors",
-        );
-        job.steps = 28;
-        job.width = 1024;
-        job.height = 1024;
-        job.cfg_scale = 7.0;
-        job.guidance = 3.5;
-        job.offload_to_cpu = false;
-        job.flash_attention_mode = FlashAttentionMode::Full;
-        job.prompt = "A Logo in white on black background saying 'Z Image' in capitals using a classic computer terminal font. Text is centered horizontally and vertically".into();
-        job
+        Self {
+            path_to_model: "/model_data/huggingface/Comfy-Org/z_image/split_files/diffusion_models/z_image_bf16.safetensors".into(),
+            path_to_vae: "/model_data/huggingface/Comfy-Org/z_image/split_files/vae/ae.safetensors".into(),
+            textencoder: ClipModel::llm("/model_data/huggingface/Comfy-Org/z_image/split_files/text_encoders/qwen_3_4b.safetensors"),
+            steps: 28,
+            width: 1024,
+            height: 1024,
+            cfg_scale: 7.0,
+            guidance: 3.5,
+            offload_to_cpu: false,
+            flash_attention_mode: FlashAttentionMode::Full,
+            prompt: "A Logo in white on black background saying 'Z Image' in capitals using a classic computer terminal font. Text is centered horizontally and vertically".into(),
+            ..Default::default()
+        }
     }
 }
