2678b6cbab39ca2ea9fca0b0658a7595ea8168cf
technose02 <technose02@yahoo.de>
2026-09-21 04:26:55 +0200

    switched text/vision encoder

--- inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/qwenimage21.rs
+++ inference-backends/src/stablediffusioncpp/stablediffusionjob/templates/qwenimage21.rs
@@ -9,9 +9,9 @@ impl StableDiffusionJob {
     pub fn qwen_image_21_job() -> Self {
         Self {
             path_to_model: "/model_data/huggingface/abenzerps/Qwen-Image-2.1-Uncensored-GGUF/qwen-image-2.1-Q4_K_M.gguf".into(),
-            text_encoder: TextEncoder::llm("/model_data/comfyui-model-base/text_encoders/Qwen3VL-8B-Instruct-Q8_0.gguf"),
             path_to_vae: "/model_data/huggingface/abenzerps/Qwen-Image-2.1-Uncensored-GGUF/vae/qwen_image_2.1_vae_bf16.safetensors".into(),
-            vision_encoder: VisionEncoder::llm_vision("/model_data/comfyui-model-base/text_encoders/mmproj-Qwen3VL-8B-Instruct-F16.gguf"),
+            text_encoder: TextEncoder::llm("/model_data/huggingface/HauhauCS/Qwen3VL-8B-Uncensored-HauhauCS-Aggressive/Qwen3VL-8B-Uncensored-HauhauCS-Aggressive-Q8_0.gguf"),
+            vision_encoder: VisionEncoder::llm_vision("/model_data/huggingface/HauhauCS/Qwen3VL-8B-Uncensored-HauhauCS-Aggressive/Qwen3VL-8B-Uncensored-HauhauCS-Aggressive-mmproj-f16.gguf"),
             cfg_scale: 6.0,
             guidance: 1.0,
             scheduler: Scheduler::Simple,
