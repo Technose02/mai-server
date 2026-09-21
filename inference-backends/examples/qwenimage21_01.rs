d13d3c3dd7f7167d2951f5fa663fa1b55cda07f0
technose02 <technose02@yahoo.de>
2026-09-21 04:27:15 +0200

    smaller image for faster processing

--- inference-backends/examples/qwenimage21_01.rs
+++ inference-backends/examples/qwenimage21_01.rs
@@ -21,8 +21,8 @@ async fn main() {
         .init();
 
     let job = StableDiffusionJob::qwen_image_21_job()
-        .with_width(2048)
-        .with_height(2048)
+        .with_width(512)
+        .with_height(512)
         .with_prompt(
             r#"
 a lovely cat
